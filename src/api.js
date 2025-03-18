396b38063f566c1045f573851ee9d03d70ea981a
alvinlaxa <alvin.r.laxa@gmail.com>
2025-03-18 10:28:03 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://bloghalalan2025.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
